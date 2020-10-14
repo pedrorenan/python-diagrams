2072bc1069ebd6de19ddc5c548eb2216ca8fcd34
Pedro Renan <pedrorenan@gmail.com>
2020-10-14 15:19:45 +0000

    Added folder to genereated diagrams and examples

--- diagram.py
+++ diagram.py
@@ -1,8 +1,90 @@
-# diagram.py
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams import Cluster, Diagram
+from diagrams.aws.compute import EC2, ECS, EKS, Lambda
+from diagrams.aws.database import ElastiCache, RDS, Redshift
+from diagrams.aws.network import ELB, Route53
+from diagrams.aws.integration import SQS
+from diagrams.aws.storage import S3
+from diagrams.gcp.analytics import BigQuery, Dataflow, PubSub
+from diagrams.gcp.compute import AppEngine, Functions
+from diagrams.gcp.database import BigTable
+from diagrams.gcp.iot import IotCore
+from diagrams.gcp.storage import GCS
 
-with Diagram("Web Service", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+diagram_name = "Grouped Workers"
+with Diagram(diagram_name, show=False, direction="TB", filename="diagrams/"+diagram_name):
+    ELB("lb") >> [EC2("worker1"),
+                  EC2("worker2"),
+                  EC2("worker3"),
+                  EC2("worker4"),
+                  EC2("worker5")] >> RDS("events")
+
+diagram_name = "Clustered Web Services"
+with Diagram(diagram_name, show=False, filename="diagrams/"+diagram_name):
+    dns = Route53("dns")
+    lb = ELB("lb")
+
+    with Cluster("Services"):
+        svc_group = [ECS("web1"),
+                     ECS("web2"),
+                     ECS("web3")]
+
+    with Cluster("DB Cluster"):
+        db_master = RDS("userdb")
+        db_master - [RDS("userdb ro")]
+
+    memcached = ElastiCache("memcached")
+
+    dns >> lb >> svc_group
+    svc_group >> db_master
+    svc_group >> memcached
+
+diagram_name = "Event Processing"
+with Diagram(diagram_name, show=False, filename="diagrams/"+diagram_name):
+    source = EKS("k8s source")
+
+    with Cluster("Event Flows"):
+        with Cluster("Event Workers"):
+            workers = [ECS("worker1"),
+                       ECS("worker2"),
+                       ECS("worker3")]
+
+        queue = SQS("event queue")
+
+        with Cluster("Processing"):
+            handlers = [Lambda("proc1"),
+                        Lambda("proc2"),
+                        Lambda("proc3")]
+
+    store = S3("events store")
+    dw = Redshift("analytics")
+
+    source >> workers >> queue >> handlers
+    handlers >> store
+    handlers >> dw
+
+diagram_name = "Message Collecting"
+with Diagram(diagram_name, show=False, filename="diagrams/"+diagram_name):
+    pubsub = PubSub("pubsub")
+
+    with Cluster("Source of Data"):
+        [IotCore("core1"),
+         IotCore("core2"),
+         IotCore("core3")] >> pubsub
+
+    with Cluster("Targets"):
+        with Cluster("Data Flow"):
+            flow = Dataflow("data flow")
+
+        with Cluster("Data Lake"):
+            flow >> [BigQuery("bq"),
+                     GCS("storage")]
+
+        with Cluster("Event Driven"):
+            with Cluster("Processing"):
+                flow >> AppEngine("engine") >> BigTable("bigtable")
+
+            with Cluster("Serverless"):
+                flow >> Functions("func") >> AppEngine("appengine")
+
+    pubsub >> flow
+
